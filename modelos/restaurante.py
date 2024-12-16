1566abff8ec08af1b10d24bd2882164bc074b0e9
limajpp <joaopedrocal90@gmail.com>
2024-12-15 21:36:15 -0300

    first commit

---
+++ modelos/restaurante.py
@@ -0,0 +1,9 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_teste = Restaurante()
+restaurante_exemplo = Restaurante()
+
+print([restaurante_teste, restaurante_exemplo])
