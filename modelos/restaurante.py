04ef0bf172458963d1fd45c3520ab4291961533a
limajpp <joaopedrocal90@gmail.com>
2024-12-16 16:59:05 -0300

    Defined the restaurant class structure

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,11 +1,29 @@
 class Restaurante:
+    restaurantes = []
+
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'Nome: {self.nome} || Categoria: {self.categoria} || Ativo: {'Sim' if self.ativo == True else 'Não'}'
+        return f'Nome: {self._nome} || Categoria: {self._categoria}'
+
+    def alternar_status(self):
+        self._ativo = not self._ativo
+
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome:'.ljust(25)} || {'Categoria:'.ljust(25)} || {'Status:'.ljust(25)}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} || {restaurante._categoria.ljust(25)} || {restaurante.ativo.ljust(25)}')
+
+    @property
+    def ativo(self):
+        return 'Ativo' if self._ativo == True else 'Inativo'
 
 restaurante_teste = Restaurante('Holy Pizza', 'Pizzaria')
-print(restaurante_teste)
+restaurante_exemplo = Restaurante('Kina', 'Comida Nordestina')
+restaurante_teste.alternar_status()
+Restaurante.listar_restaurantes()
