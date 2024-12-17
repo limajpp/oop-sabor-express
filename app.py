153ee913a17845b23519fc92f7ca4a73164d76f5
limajpp <joaopedrocal90@gmail.com>
2024-12-17 14:54:45 -0300

    feat: 'Added menu classes'

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_teste = Restaurante('Holy Pizza', 'Pizzaria')
-restaurante_teste.alternar_status()
 
-restaurante_teste.receber_avaliacao('Gui', 10)
-restaurante_teste.receber_avaliacao('Lais', -4)
-restaurante_teste.receber_avaliacao('Emy', 4.3)
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
+bebida_suco.aplicar_desconto()
+
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor da cidade')
+prato_paozinho.aplicar_desconto()
+
+restaurante_teste.adicionar_ao_cardapio([bebida_suco, prato_paozinho])
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_teste.exibir_o_cardapio
 
 if __name__ == '__main__':
     main()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,4 +1,5 @@
 from modelos.avaliacao import Avaliacao
+from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
     """Representa um restaurante e suas características."""
@@ -17,6 +18,7 @@ class Restaurante:
         self._categoria = categoria.upper()
         self._ativo = False
         self._avaliacao = []
+        self._cardapio = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -59,3 +61,18 @@ class Restaurante:
         quantidade_de_notas = len(self._avaliacao)
         
         return round(soma_das_notas / quantidade_de_notas, 1)
+    
+    def adicionar_ao_cardapio(self, itens: list):
+        self._cardapio.extend(item for item in itens if isinstance(item, ItemCardapio))
+
+    @property
+    def exibir_o_cardapio(self):
+        print(f'Cardápio do restaurante {self._nome}\n')
+        for i, item in enumerate(self._cardapio, start=1):
+            if hasattr(item, '_descricao'):
+                mensagem_prato = f'{i}. Nome: {item._nome} | Preço: {item._preco} | Descrição: {item._descricao}'
+                print(mensagem_prato)
+            else:
+                mensagem_bebida = f'{i}. Nome: {item._nome} | Preço: {item._preco} | Tamanho: {item._tamanho}'
+                print(mensagem_bebida)
+
